c04b78f487c7a57628b88028113674616ba8ffd4
Jhonata <jhonatafontesluis23@gmail.com>
2025-03-22 23:32:45 -0300

    Novos templates de cliente.html, create.html, método no controller de novo cliente para a pagina e ação criar
    Signed-off-by: Jhonata <jhonatafontesluis23@gmail.com>

--- src/main/java/com/bmt/webApp/controller/ClienteController.java
+++ src/main/java/com/bmt/webApp/controller/ClienteController.java
@@ -4,10 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bmt.webApp.model.ClienteDto;
 import com.bmt.webApp.repository.ClienteRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/clients")
@@ -20,6 +26,18 @@ public class ClienteController {
     public String getClients(Model model){
         var clients = clienteRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
         model.addAttribute("clients", clients);
-        return "clients/index";
+        return "cliente/cliente";
+    }
+
+    @GetMapping("/create")
+    public String CreateClient(Model model){
+        ClienteDto clienteDto = new ClienteDto();
+        model.addAttribute("clienteDto", clienteDto);
+        return "cliente/create";
+    }
+
+    @PostMapping("/create")
+    public String createClient(@Valid @ModelAttribute ClienteDto clienteDto, BindingResult result){
+        return "redirect:/cliente";
     }
 }
