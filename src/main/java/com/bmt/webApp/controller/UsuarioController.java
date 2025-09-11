e4e977877b1d96bf4746766a64c0c411778f15d1
Jhonata <jhonatafontesluis23@gmail.com>
2025-09-11 14:45:52 -0300

    update messages of return of notification
    Signed-off-by: Jhonata <jhonatafontesluis23@gmail.com>

--- src/main/java/com/bmt/webApp/controller/UsuarioController.java
+++ src/main/java/com/bmt/webApp/controller/UsuarioController.java
@@ -91,25 +91,30 @@ public class UsuarioController {
 
     @PostMapping("/update")
     public String updateUser(@Valid @ModelAttribute Usuario user,
-                             BindingResult result, RedirectAttributes redirect,
+                             BindingResult result, 
+                             RedirectAttributes redirect,
                              Model model) {
 
         // Verifica se há erros de validação
         if(result.hasErrors()){
             // Retorna para a página de edição, mantendo os dados e os erros
             model.addAttribute("funcoes", Funcao.values());
+            model.addAttribute("user", user);
             return "usuario/edit";
         }
 
         try {
             // Atualiza o usuário através do serviço
             userService.updateUser(user);
+            redirect.addFlashAttribute("successMessage", "Employer atualizado com Sucesso!");
         } catch (IllegalArgumentException e) {
             // Se o usuário não existir, adiciona um erro ao resultado
             result.rejectValue("email", "error.email", e.getMessage());
             model.addAttribute("funcoes", Funcao.values());
+            model.addAttribute("user", user);
             return "usuario/edit";
         }
+        
         // Se a atualização for bem-sucedida, adiciona uma mensagem de sucesso
         redirect.addFlashAttribute("successMessage", "Employer atualizado com Sucesso!");
         return "redirect:/users";
